4e80c0d83590c75205c5c861afcc5909683577c6
Joydeep Dey <dey.joydeep.cse@gmail.com>
2018-03-23 18:07:17 +0900

    updated

--- src/main/java/com/jd/app/config/SpringConfiguration.java
+++ src/main/java/com/jd/app/config/SpringConfiguration.java
@@ -1,11 +1,39 @@
 package com.jd.app.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.View;
+import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.json.MappingJackson2JsonView;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages = "com.jd.app")
-public class SpringConfiguration {
+public class SpringConfiguration implements WebMvcConfigurer {
+
+	@Bean
+	public View jsonTemplate() {
+		MappingJackson2JsonView view = new MappingJackson2JsonView();
+		view.setPrettyPrint(true);
+		return view;
+	}
+
+	@Bean
+	public ViewResolver viewResolver() {
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		viewResolver.setPrefix("/WEB-INF/views/");
+		viewResolver.setSuffix(".jsp");
+		return viewResolver;
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/resources/**")
+				.addResourceLocations("/resources/").setCachePeriod(31556926);
+	}
 }
