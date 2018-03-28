0c4685c1d7d0bb35ceec8709c3ee12f06eba9a7f
Joydeep Dey <dey.joydeep.cse@gmail.com>
2018-03-28 18:35:23 +0900

    updated

--- src/main/java/com/jd/app/beans/prelogin/SignupBean.java
+++ src/main/java/com/jd/app/beans/prelogin/SignupBean.java
@@ -1,103 +1,35 @@
 package com.jd.app.beans.prelogin;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
 import com.jd.app.beans.common.SharedBean;
 
+@ToString
 public class SignupBean extends SharedBean {
 
+	@Getter
+	@Setter
 	private String email;
+
+	@Getter
+	@Setter
 	private String password;
+
+	@Getter
+	@Setter
 	private String firstname;
+
+	@Getter
+	@Setter
 	private String lastname;
+
+	@Getter
+	@Setter
 	private String birthdate;
+
+	@Getter
+	@Setter
 	private String sex;
-
-	/**
-	 * @return the email
-	 */
-	public final String getEmail() {
-		return email;
-	}
-
-	/**
-	 * @param email
-	 *            the email to set
-	 */
-	public final void setEmail(String email) {
-		this.email = email;
-	}
-
-	/**
-	 * @return the password
-	 */
-	public final String getPassword() {
-		return password;
-	}
-
-	/**
-	 * @param password
-	 *            the password to set
-	 */
-	public final void setPassword(String password) {
-		this.password = password;
-	}
-
-	/**
-	 * @return the firstname
-	 */
-	public final String getFirstname() {
-		return firstname;
-	}
-
-	/**
-	 * @param firstname
-	 *            the firstname to set
-	 */
-	public final void setFirstname(String firstname) {
-		this.firstname = firstname;
-	}
-
-	/**
-	 * @return the lastname
-	 */
-	public final String getLastname() {
-		return lastname;
-	}
-
-	/**
-	 * @param lastname
-	 *            the lastname to set
-	 */
-	public final void setLastname(String lastname) {
-		this.lastname = lastname;
-	}
-
-	/**
-	 * @return the birthdate
-	 */
-	public final String getBirthdate() {
-		return birthdate;
-	}
-
-	/**
-	 * @param birthdate
-	 *            the birthdate to set
-	 */
-	public final void setBirthdate(String birthdate) {
-		this.birthdate = birthdate;
-	}
-
-	/**
-	 * @return the sex
-	 */
-	public final String getSex() {
-		return sex;
-	}
-
-	/**
-	 * @param sex
-	 *            the sex to set
-	 */
-	public final void setSex(String sex) {
-		this.sex = sex;
-	}
 }
