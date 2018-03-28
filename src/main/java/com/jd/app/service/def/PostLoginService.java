44124408ac22dabf2ba832f4494a205ad71415ea
Joydeep Dey <dey.joydeep.cse@gmail.com>
2018-03-28 18:37:11 +0900

    updated

--- src/main/java/com/jd/app/service/def/PostLoginService.java
+++ src/main/java/com/jd/app/service/def/PostLoginService.java
@@ -2,6 +2,8 @@ package com.jd.app.service.def;
 
 import java.util.List;
 
+import com.jd.app.beans.postlogin.FrameBean;
+import com.jd.app.beans.postlogin.MenuBean;
 import com.jd.app.beans.postlogin.MessageBean;
 import com.jd.app.beans.postlogin.UserDetailsBean;
 
@@ -11,5 +13,9 @@ public interface PostLoginService {
 
 	List<MessageBean> loadOldMessages(String loginId, int currentSize,
 			int nextSize);
+
+	List<MenuBean> getMenus();
+
+	List<FrameBean> getFrames();
 
 }
