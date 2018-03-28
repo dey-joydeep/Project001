c5bb08310b040a8c88d0ffbb41aa65f9a737a326
Joydeep Dey <dey.joydeep.cse@gmail.com>
2018-03-28 18:38:12 +0900

    updated

--- src/main/java/com/jd/app/controller/PostLoginController.java
+++ src/main/java/com/jd/app/controller/PostLoginController.java
@@ -8,6 +8,8 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.jd.app.beans.postlogin.FrameBean;
+import com.jd.app.beans.postlogin.MenuBean;
 import com.jd.app.beans.postlogin.MessageBean;
 import com.jd.app.beans.postlogin.UserDetailsBean;
 import com.jd.app.service.def.PostLoginService;
@@ -30,5 +32,17 @@ public class PostLoginController {
 			@RequestParam int currentSize, @RequestParam int nextSize) {
 		return postLoginService.loadOldMessages(loginId, currentSize, nextSize);
 
+	}
+
+	@GetMapping(value = "/menu", produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public List<MenuBean> loadMenus() {
+		return postLoginService.getMenus();
+	}
+
+	@GetMapping(value = "/frame", produces = "application/json; charset=UTF-8")
+	@ResponseBody
+	public List<FrameBean> loadFrames() {
+		return postLoginService.getFrames();
 	}
 }
