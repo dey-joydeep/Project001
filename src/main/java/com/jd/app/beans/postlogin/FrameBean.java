2e94c0ae3c6753fc569d185515a8a6182f51985f
Joydeep Dey <dey.joydeep.cse@gmail.com>
2018-03-28 18:45:22 +0900

    added

---
+++ src/main/java/com/jd/app/beans/postlogin/FrameBean.java
@@ -0,0 +1,17 @@
+package com.jd.app.beans.postlogin;
+
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
+public class FrameBean {
+
+	@Getter
+	@Setter
+	private String frameId;
+
+	@Getter
+	@Setter
+	private String frameSource;
+}
