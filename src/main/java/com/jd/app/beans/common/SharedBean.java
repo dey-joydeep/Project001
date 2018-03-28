01b883ea2d08a2ddf8dfdbca9030ec801f03ce50
Joydeep Dey <dey.joydeep.cse@gmail.com>
2018-03-28 18:31:09 +0900

    updated

--- src/main/java/com/jd/app/beans/common/SharedBean.java
+++ src/main/java/com/jd/app/beans/common/SharedBean.java
@@ -1,24 +1,21 @@
 package com.jd.app.beans.common;
 
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@ToString
 public class SharedBean {
 
-	private Boolean status;
+	@Getter
+	@Setter
+	private boolean success;
 
+	@Getter
+	@Setter
+	private int status;
+
+	@Getter
+	@Setter
 	private String message;
-
-	public Boolean getStatus() {
-		return status;
-	}
-
-	public void setStatus(Boolean status) {
-		this.status = status;
-	}
-
-	public String getMessage() {
-		return message;
-	}
-
-	public void setMessage(String message) {
-		this.message = message;
-	}
 }
