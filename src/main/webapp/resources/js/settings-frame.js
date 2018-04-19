8fc500416037607f8778b2aec346636bc9848dfd
Joydeep Dey <dey.joydeep.cse@gmail.com>
2018-04-19 17:50:03 +0900

    updated

--- src/main/webapp/resources/js/settings-frame.js
+++ src/main/webapp/resources/js/settings-frame.js
@@ -4,18 +4,9 @@ $(function() {
 		clickLink(dataParam);
 		top.window.location.hash = this.hash;
 	});
-	setUsernameSettignsButtonValue();
 });
 
 function clickLink(param) {
 	$('.details').hide();
 	$('div[data-param="' + param + '"]').show();
-}
-
-function setUsernameSettignsButtonValue() {
-	var username = $(this.parent.document).find('#h-uname').val();
-	if (username.startsWith('$'))
-		$('#uname-set-btn').val("Set");
-	else
-		$('#uname-set-btn').val("Update");
 }
